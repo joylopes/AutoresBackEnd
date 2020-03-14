ccae2222707054ca738bf107ac1ac143db2c53ad
Joice Lopes <junior@techdog.com.br>
2020-03-14 08:20:04 -0300

    + ContextFactory criado e configurado

---
+++ src/ProjetoAutores.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ProjetoAutores.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado p/ criar as migrações
+            var connectionString = "Server=localhost;Port=3306;Database=DbProjetoAutores;Uid=sa;Pwd=acsj1996";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
